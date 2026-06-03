092b97f4e2501f0aa336e4fe62e879a2975d04cb
panther706 <43943311+panther706@users.noreply.github.com>
2026-06-03 22:36:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,17 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+#修复Rust编译失败
+#sed -i 's/ci-llvm=true/ci-llvm=false/g' feeds/packages/lang/rust/Makefile
+wget -O feeds/packages/lang/rust/Makefile https://raw.githubusercontent.com/immortalwrt/packages/refs/heads/master/lang/rust/Makefile
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 添加上网插件
+git clone --depth=1 https://github.com/vernesong/OpenClash.git package/openclash
+git clone --depth=1 https://github.com/panther706/NIKKI.git package/nikki
+
+#添加插件
+git clone --depth=1 https://github.com/timsaya/luci-app-bandix.git package/luci-app-bandix
+git clone --depth=1 https://github.com/timsaya/openwrt-bandix.git package/openwrt-bandix
+git clone --depth=1 https://github.com/panther706/luci-app-adguardhome.git package/luci-app-adguardhome
